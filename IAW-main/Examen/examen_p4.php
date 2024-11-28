8823741c2e7f7df6ab7106fabedcc0eae57a4fe1
marquitobrrr <mmrivero13@gmail.com>
2024-11-28 13:33:06 +0100

    looo diablo

--- IAW-main/Examen/examen_p4.php
+++ IAW-main/Examen/examen_p4.php
@@ -6,40 +6,53 @@
     <style>
         body {
             display: flex;
-            justify-content: center;
+            flex-direction: column;
             align-items: center;
+            justify-content: center;
             height: 100vh;
+            margin: 0;
             text-align: center;
         }
-        h1 {
+        .premio {
             font-size: 5em;
+            margin: 20px 0;
         }
-        .enhorabuena {
-            font-size: 2em;
+        .mensaje {
+            font-size: 2em; 
+            margin: 20px 0;
+        }
+        button {
+            padding: 10px 20px;
+            font-size: 1.5em;
         }
     </style>
 </head>
 <body>
+    <h1>Bienvenido al mundo de los Pitufos</h1>
     <?php
-    $nombre = htmlspecialchars($_POST['nombre']);
-    $apellidos = htmlspecialchars($_POST['apellidos']);
-    $edad = htmlspecialchars($_POST['edad']);
-    $suerte = htmlspecialchars($_POST['suerte']);
-    $premio = 100 * $suerte * rand(1, 5); 
+    function calcularPremio($suerte) {
+        $numeroAleatorio = rand(1, 10000);
+        $multiplicador = rand(1, 5);
+        return [
+            'numeroAleatorio' => $numeroAleatorio,
+            'premio' => 100 * $suerte * $multiplicador
+        ];
+    }
+
+    if (isset($_POST['suerte'])) {
+        $suerte = intval($_POST['suerte']); 
+
+        $resultado = calcularPremio($suerte);
+        $premio = $resultado['premio'];
+        $mensaje = $premio > 3000 ? "Enhorabuena de la buena!" : "Enhoramala de la mala!";
     ?>
-    
-    <h1><?php echo $premio; ?></h1>
-    <?php if ($premio > 3000): ?>
-        <div class="enhorabuena">¡Enhorabuena de la buena!</div>
-    <?php endif; ?>
-    
-    <form action="index.php" method="post">
-        <input type="hidden" name="nombre" value="<?php echo $nombre; ?>">
-        <input type="hidden" name="apellidos" value="<?php echo $apellidos; ?>">
-        <input type="hidden" name="edad" value="<?php echo $edad; ?>">
-        <input type="hidden" name="suerte" value="<?php echo $suerte; ?>">
-        
-        <button type="submit">Probar suerte de nuevo</button>
-    </form>
+        <div class="premio">Tu premio es: <?php echo $premio; ?></div>
+        <div class="mensaje"><?php echo $mensaje; ?></div>
+    <?php
+    } else {
+        echo "<p>No se ha recibido el número de la suerte. Por favor, vuelve a intentarlo.</p>";
+    }
+    ?>
+    <button onclick="window.location.reload();">Probar suerte de nuevo</button>
 </body>
 </html>
