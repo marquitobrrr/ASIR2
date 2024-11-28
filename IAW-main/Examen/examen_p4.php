76d8c2473a5cdf8462e6c88b415ab725cdba54ff
marquitobrrr <mmrivero13@gmail.com>
2024-11-28 12:31:20 +0100

    ggggg

--- IAW-main/Examen/examen_p4.php
+++ IAW-main/Examen/examen_p4.php
@@ -21,3 +21,28 @@
 </head>
 <body>
     <?php
+    // Recuperar datos del formulario anterior
+    $nombre = htmlspecialchars($_POST['nombre']);
+    $apellidos = htmlspecialchars($_POST['apellidos']);
+    $edad = htmlspecialchars($_POST['edad']);
+    $suerte = htmlspecialchars($_POST['suerte']);
+    
+    // Calcular el premio
+    $premio = 100 * $suerte * rand(1, 5); // Premio aleatorio basado en el número de la suerte
+    ?>
+    
+    <h1><?php echo $premio; ?></h1>
+    <?php if ($premio > 3000): ?>
+        <div class="enhorabuena">¡Enhorabuena de la buena!</div>
+    <?php endif; ?>
+    
+    <form action="index.php" method="post">
+        <input type="hidden" name="nombre" value="<?php echo $nombre; ?>">
+        <input type="hidden" name="apellidos" value="<?php echo $apellidos; ?>">
+        <input type="hidden" name="edad" value="<?php echo $edad; ?>">
+        <input type="hidden" name="suerte" value="<?php echo $suerte; ?>">
+        
+        <button type="submit">Probar suerte de nuevo</button>
+    </form>
+</body>
+</html>
